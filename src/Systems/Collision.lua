143f2f7fbc05d796b10155dac0a91966b52ea3b1
Benj Brimeyer <bbrimeyer@roblox.com>
2019-08-10 22:52:03 -0700

    remove print lol

--- src/Systems/Collision.lua
+++ src/Systems/Collision.lua
@@ -5,7 +5,6 @@ local newSystem = World.System:extend(script.Name)
 function newSystem:step(t, dt)
 	for entity, canCollide, transform in World.core:components("CanCollide", "Transform") do
 		--TODO:
-		print("GOT ENTITY:", entity)
 	end
 end
 
